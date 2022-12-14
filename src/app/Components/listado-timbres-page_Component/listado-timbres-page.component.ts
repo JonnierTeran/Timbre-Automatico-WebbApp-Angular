fe544a0e5b6e1760018ca1b9b86c2ac7ea414856
Jonnier Andres Teran Morales <jonnierteranmorales@gmail.com>
2022-12-14 17:14:55 -0500

    Libreria mqtt

--- src/app/Components/listado-timbres-page_Component/listado-timbres-page.component.ts
+++ src/app/Components/listado-timbres-page_Component/listado-timbres-page.component.ts
@@ -10,6 +10,17 @@ import { CalendarModel } from 'src/app/Models/Calendar.models';
 //Libreria de alertas
 import Swal from 'sweetalert2';
 
+//Librerias mqtt
+import {
+  IMqttMessage,
+  IMqttServiceOptions,
+  MqttService,
+  IPublishOptions,
+} from 'ngx-mqtt';
+import { IClientSubscribeOptions } from 'mqtt-browser';
+import { Subscription } from 'rxjs';
+
+
 @Component({
   selector: 'app-listado-timbres-page',
   templateUrl: './listado-timbres-page.component.html',
@@ -100,5 +111,95 @@ export class ListadoTimbresPageComponent {
         this._CalendarService.DeleteCalendar(Pos, Id);
       }
     });
+  }
+
+
+  /************************************************************************************* */
+  
+  private curSubscription: Subscription | undefined;
+  connection = {
+    hostname: '192.168.100.231',
+    port: 9001,
+    //path: '/mqtt',
+    //clean: true, // Retain session
+    //connectTimeout: 4000, // Timeout period
+    //reconnectPeriod: 4000, // Reconnect period
+    // Authentication information
+    clientId: 'mqttx_597046f4',
+    username: 'emqx_test',
+    password: 'emqx_test',
+    protocol: 'ws',
+  }
+  subscription = {
+    topic: 'cmnd/rasmota/Power',
+    qos: 0,
+  };
+  publish = {
+    topic: 'cmnd/tasmota/Power',
+    qos: 0,
+    payload: 'ON',
+  };
+  off = {
+    topic: 'cmnd/tasmota/Power',
+    qos: 0,
+    payload: 'OFF',
+  };
+  receiveNews = '';
+  qosList = [
+    { label: 0, value: 0 },
+    { label: 1, value: 1 },
+    { label: 2, value: 2 },
+  ];
+  client: MqttService | undefined;
+  isConnection = false;
+  subscribeSuccess = false;
+
+  // Create a connection
+  createConnection() {
+    // Connection string, which allows the protocol to specify the connection method to be used
+    // ws Unencrypted WebSocket connection
+    // wss Encrypted WebSocket connection
+    // mqtt Unencrypted TCP connection
+    // mqtts Encrypted TCP connection
+    try {
+      this.client?.connect(this.connection as IMqttServiceOptions)
+    } catch (error) {
+      console.log('mqtt.connect error', error);
+    }
+    this.client?.onConnect.subscribe(() => {
+      this.isConnection = true
+      console.log('Connection succeeded!');
+    });
+    this.client?.onError.subscribe((error: any) => {
+      this.isConnection = false
+      console.log('Connection failed', error);
+    });
+    this.client?.onMessage.subscribe((packet: any) => {
+      this.receiveNews = this.receiveNews.concat(packet.payload.toString())
+      console.log(`Received message ${packet.payload.toString()} from topic ${packet.topic}`)
+    })
+  }
+
+  // 订阅主题
+  doSubscribe() {
+    const { topic, qos } = this.subscription
+    this.curSubscription = this.client?.observe(topic, { qos } as IClientSubscribeOptions).subscribe((message: IMqttMessage) => {
+      this.subscribeSuccess = true
+      console.log('Subscribe to topics res', message.payload.toString())
+    })
+  }
+  // 取消订阅
+ 
+  // 发送消息
+  doPublish() {
+    const { topic, qos, payload } = this.publish
+    console.log(this.publish)
+    this.client?.unsafePublish(topic, payload, { qos } as IPublishOptions)
+  }
+
+  doOff() {
+    const { topic, qos, payload } = this.off
+    console.log(this.publish)
+    this.client?.unsafePublish(topic, payload, { qos } as IPublishOptions)
   }
 }
